767818f998fa8921dbc60a9f3e434a58af2d2190
lanx7 <lanx7@naver.com>
2016-02-18 10:08:21 +0900

    Linear regression initially working

--- _test_linear_regression.py
+++ _test_linear_regression.py
@@ -7,7 +7,6 @@ y = data[:,1]
 print X, np.shape(X)
 print y, np.shape(y)
 
-
 m = np.size(X)
 X = np.column_stack((np.ones(m),X))
 theta = np.zeros(2)
@@ -15,3 +14,6 @@ print X
 print computeCost(X, y, theta)
 
 
+alpha = 0.005
+numIteration = 1000
+theta = gradientDescent(X, y, theta, alpha, numIteration)
--- linear_regression.py
+++ linear_regression.py
@@ -3,28 +3,30 @@ import numpy as np
 def computeCost(X, y, theta):
     hypothesis = np.dot(X, theta)                   # (m by n) * (n by 1) --> (m by 1)
     diff = hypothesis - y                           # (m by 1) - (m by 1)
-    cost = sum(np.square(diff)) / (2 *np.size(y))   # (m by 1)  --> Scala, np.square: element-wise ^2
+    #cost = sum(np.square(diff)) / (2 *np.size(y))   # (m by 1)  --> Scala, np.square: element-wise ^2
+    cost = np.sum(diff ** 2) / (2 *np.size(y))
     return cost
     pass
 
 
-def gradientDescent(x, y, theta, alpha, m, numIterations):
+def gradientDescent(X, y, theta, alpha, numIterations):
+    m = np.size(y)
+    cost_history = np.zeros(numIterations)
     #print np.shape(x), np.shape(y), np.shape(theta)
-    xTrans = x.transpose()
-    #print np.shape(xTrans)
     for i in range(0, numIterations):
-        hypothesis = np.dot(x, theta)
-        loss = hypothesis - y
+        hypothesis = np.dot(X, theta)
+        diff = hypothesis - y
         # avg cost per example (the 2 in 2*m doesn't really matter here.
         # But to be consistent with the gradient, I include it)
-        cost = np.sum(loss ** 2) / (2 * m)
-            #print("Iteration %d | Cost: %f" % (i, cost))
+        cost = np.sum(diff ** 2) / (2 * m)
+        print("Iteration %d | Cost: %f" % (i, cost))
         # avg gradient per example
         # gradient = np.dot(xTrans, loss) / m
-        gradient = np.dot(loss.transpose(), x) / m
+        gradient = np.dot(X.T, diff) / m
         # update
         theta = theta - alpha * gradient
-            #print ("Theta: %f", theta)
+
+    print ("Theta: %f", theta)
     return theta
 
 def gradRegres(xArr, yArr):
