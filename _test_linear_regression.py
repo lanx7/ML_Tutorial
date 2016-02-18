4eded8fce7553bae65eb628387ee2e36b0a3aa28
Tommy <Tommy>
2016-02-18 11:59:47 +0900

    Linear Regression for single feature

--- _test_linear_regression.py
+++ _test_linear_regression.py
@@ -1,19 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from linear_regression import *
+import os
 
-data = np.genfromtxt('data/ex1data1.txt', delimiter=',')
+PLOT_MODE = True
+
+# Linear regresstion test for data1 (Single Feature)
+data = np.genfromtxt('data/lr_data1.txt', delimiter=',')
 X = data[:,0]
 y = data[:,1]
-print X, np.shape(X)
-print y, np.shape(y)
 
 m = np.size(X)
 X = np.column_stack((np.ones(m),X))
 theta = np.zeros(2)
-print X
-print computeCost(X, y, theta)
 
-
-alpha = 0.005
+alpha = 0.0001
 numIteration = 1000
-theta = gradientDescent(X, y, theta, alpha, numIteration)
+print "Training theta......."
+theta, cost_history = gradientDescent(X, y, theta, alpha, numIteration)
+print "Theta is converged to :", theta
+
+if PLOT_MODE == True:
+    # Scattering Data
+    plt.scatter(X[:,1], y, marker="x")
+    plt.ylabel('Profit in $10,000s')
+    plt.xlabel('Population of City in 10,000s')
+
+    # Regression Line
+    plt.plot(X[:,1], np.dot(X,theta),'r')
+
+    # Cost(J) changes as the learning goes
+    plt.figure()
+    plt.plot(range(0,numIteration), cost_history)
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Cost(J)")
+    plt.show()
--- linear_regression.py
+++ linear_regression.py
@@ -1,50 +1,23 @@
 import numpy as np
 
-def computeCost(X, y, theta):
-    hypothesis = np.dot(X, theta)                   # (m by n) * (n by 1) --> (m by 1)
-    diff = hypothesis - y                           # (m by 1) - (m by 1)
-    #cost = sum(np.square(diff)) / (2 *np.size(y))   # (m by 1)  --> Scala, np.square: element-wise ^2
-    cost = np.sum(diff ** 2) / (2 *np.size(y))
-    return cost
-    pass
-
-
 def gradientDescent(X, y, theta, alpha, numIterations):
     m = np.size(y)
     cost_history = np.zeros(numIterations)
-    #print np.shape(x), np.shape(y), np.shape(theta)
     for i in range(0, numIterations):
-        hypothesis = np.dot(X, theta)
-        diff = hypothesis - y
-        # avg cost per example (the 2 in 2*m doesn't really matter here.
-        # But to be consistent with the gradient, I include it)
-        cost = np.sum(diff ** 2) / (2 * m)
+        hypothesis = np.dot(X, theta)            # (m by n) * (n by 1) --> (m by 1)
+        diff = hypothesis - y                    # (m by 1) - (m by 1)
+
+        # avg cost per example, cost itself is not used for updating theta.
+        # cost_history is useful for debugging the learning rate
+        cost = np.sum(diff ** 2) / (2 * m)      # m by 1 -> Scala by vector summation
+        cost_history[i] = cost
         print("Iteration %d | Cost: %f" % (i, cost))
-        # avg gradient per example
-        # gradient = np.dot(xTrans, loss) / m
+
+        # avg gradient per example, (n by m) * (m by 1) --> n by 1 vector (i.e., feature-wise vector)
         gradient = np.dot(X.T, diff) / m
-        # update
+
+        # update theta
         theta = theta - alpha * gradient
 
-    print ("Theta: %f", theta)
-    return theta
-
-def gradRegres(xArr, yArr):
-    xTemp = np.mat(xArr)
-    yTemp = np.mat(yArr)
-    xMat = np.squeeze(np.asarray(xTemp))
-    yMat = np.squeeze(np.asarray(yTemp))
-    #xMat = xTemp.getA()
-    #yMat = yTemp.getA()
-    #yMat = np.ones(m) * yArr #array scalar multiplication
-
-    m, n = np.shape(xMat) #m = 100, n = 2
-
-    #print yMat
-    theta = np.ones(n)
-    alpha = 0.0005
-    numIteration = 100000
-    theta = gradientDescent(xMat, yMat, theta, alpha, m, numIteration)
-    #theta = gradientDescentMatrix(xMat, yMat, theta, alpha, m, numIteration)
-    return theta
-
+    #print ("Theta: %f", theta)
+    return theta, cost_history
